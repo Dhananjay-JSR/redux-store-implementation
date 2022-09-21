06a41b427900263368a05c11c74f4dc2bf33fd7b
Dhananjay-JSR <ajaysenday@gmail.com>
2022-09-21 20:51:29 +0530

    React Persists Implementation

--- src/ReduxStore/store.ts
+++ src/ReduxStore/store.ts
@@ -1,10 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore ,combineReducers} from '@reduxjs/toolkit'
 import CartSlice from './CartSlice'
 import DataSlice from './DataSlice'
+// @ts-ignore
+import storage from 'redux-persist/lib/storage'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
+import thunk from 'redux-thunk';
 
-export default configureStore({
-  reducer: {
-    Data:DataSlice ,
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const rootReducer = combineReducers({
+  Data:DataSlice ,
     Cart:CartSlice
-  },
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+})
+
+export const persistor = persistStore(store)
--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { Provider } from 'react-redux'
 import App from './App'
-import store from './ReduxStore/store'
+import {store} from './ReduxStore/store'
+import { persistor } from './ReduxStore/store'
+import { PersistGate } from 'redux-persist/integration/react'
 // import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
         <Provider store={store} >
-
+        <PersistGate loading={null} persistor={persistor}>
     <App />
+    </PersistGate>
         </Provider>
   </React.StrictMode>
 )
